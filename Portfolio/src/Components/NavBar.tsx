bdddff0fca40d60d4df3d5e839d79803bb33af46
Synkrotic <Synkrotic>
2025-03-07 14:28:54 +0000

    Navbar Working including free movement of the navbar.

--- Portfolio/src/Components/NavBar.tsx
+++ Portfolio/src/Components/NavBar.tsx
@@ -1,20 +1,68 @@
-import { AiOutlineHome, AiFillHome, AiOutlineUser, AiFillUpSquare, AiOutlineProduct, AiFillProduct, AiOutlineTool, AiTwotoneTool } from 'react-icons/ai'
+import {
+  AiOutlineHome, AiFillHome,
+  AiOutlineUser, AiFillUpSquare,
+  AiOutlineProduct, AiFillProduct,
+  AiOutlineTool, AiTwotoneTool,
+  AiFillGithub, AiOutlineGithub,
+} from 'react-icons/ai'
+import { Move } from 'react-feather'
+
 import './Stylings/navbar.css'
 import NavBarItem from './NavBarItem'
+import React from 'react'
 
-const NavBar = () => {
-  return (
-    <div className='navbar-container'>
-      <NavBarItem Icon={AiOutlineHome} title='Home' />
-      <NavBarItem Icon={AiFillHome} title='Home' />
-      <NavBarItem Icon={AiOutlineUser} title='About Me' />
-      <NavBarItem Icon={AiFillUpSquare} title='About Me' />
-      <NavBarItem Icon={AiOutlineProduct} title='Projects' />
-      <NavBarItem Icon={AiFillProduct} title='Projects' />
-      <NavBarItem Icon={AiOutlineTool} title='Settings' />
-      <NavBarItem Icon={AiTwotoneTool} title='Settings' />
-    </div> 
-  )
+class NavBar extends React.Component {
+  navItemValues: Array<any>;
+  navItems: Array<any>;
+  moveSpeed: number = 0.5;
+  moveable: boolean = false;
+  constructor(props: any) {
+    super(props)
+    this.navItemValues = [
+      { onHover: this.setProximity, IconFill: AiFillHome, IconOutline: AiOutlineHome, title: 'Home' },
+      { onHover: this.setProximity, IconFill: AiFillUpSquare, IconOutline: AiOutlineUser, title: 'About Me' },
+      { onHover: this.setProximity, IconFill: AiFillProduct, IconOutline: AiOutlineProduct, title: 'Projects' },
+      { onHover: this.setProximity, IconFill: AiFillGithub, IconOutline: AiOutlineGithub, title: 'Github' },
+      { onHover: this.setProximity, IconFill: AiTwotoneTool, IconOutline: AiOutlineTool, title: 'Settings' },
+    ]
+    this.navItems = this.navItemValues.map((item, index) => (
+      <NavBarItem key={index} {...item} id={index} />
+    ))
+  }
+
+  setProximity(hoverID: number) {
+    this.navItems.forEach((item: any) => item.setProximity(false));
+    this.navItems[hoverID + 1].setProximity(true);
+  }
+
+  moveNavBar(e: React.MouseEvent) {
+    if (!this.moveable) return;
+    let mouseCoords = { x: e.clientX, y: e.clientY };
+    const navbar = document.getElementsByClassName('navbar-container')[0] as HTMLElement;
+    navbar.style.left = `${mouseCoords.x + 1920*(28/1920)}px`;
+    navbar.style.top = `${mouseCoords.y - 1080*(39/1080)}px`;
+  }
+
+  render() {
+    return (
+      <div
+        className='navbar-wrapper'
+        onMouseUp={() => this.moveable = false}
+        onMouseMove={(e) => this.moveNavBar(e)}
+      >
+        <nav className='navbar-container left'
+          >
+          <button 
+            className='navbar-item nohover purple-hover' 
+            onMouseDown={() => this.moveable = true}
+          >
+            <Move />
+          </button>
+          {this.navItems}
+        </nav>
+      </div>
+    )
+  }
 }
 
 export default NavBar
--- Portfolio/src/Components/NavBarItem.tsx
+++ Portfolio/src/Components/NavBarItem.tsx
@@ -1,15 +1,47 @@
+import React from 'react';
+
 interface NavBarItemProps {
-  Icon: any,
+  onHover: (id: number) => void,
+  IconFill: any,
+  IconOutline: any,
   title: string,
+  id: number,
 }
 
-const NavBarItem = ({ Icon, title }: NavBarItemProps) => {
-  return (
-    <button className='navbar-item focus'>
-        <Icon className="icon" />
-        <span>{title}</span>
-    </button>
-  )
+class NavBarItem extends React.Component<NavBarItemProps> {
+  IconFill: any;
+  IconOutline: any;
+  Icon: any;
+  proximity: boolean;
+  title: string;
+  id: number;
+  onHover: (id: number) => void;
+  constructor(props: NavBarItemProps) {
+    super(props);
+    this.IconFill = props.IconFill;
+    this.IconOutline = props.IconOutline;
+    this.Icon = this.IconOutline;
+    this.proximity = false;
+    this.title = props.title;
+    this.id = props.id;
+    this.onHover = props.onHover;
+  }
+
+  switchState() {
+    console.log('Switching state');
+    this.Icon = this.Icon === this.IconOutline ? this.IconFill : this.IconOutline;
+  }
+
+  setProximity(state: boolean) { this.proximity = state; }
+
+  render() {
+    return (
+      <button className={`navbar-item ${this.proximity ? 'proximity' : ''}`} onClick={() => this.switchState()} onMouseEnter={() => this.onHover(this.id)}>
+        <this.Icon className="icon" />
+        <span>{this.title}</span>
+      </button>
+    );
+  }
 }
 
 export default NavBarItem
