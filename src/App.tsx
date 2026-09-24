d3d29f01d487e98e51f1655038554f94e16b15d0
Johannes L. Borger <jannesborger@outlook.com>
2026-09-24 21:33:23 +0200

    added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="*" element={<h1>404</h1>} />
 				<Route path="/" element={<Homepage />} />
 				<Route path="/voorraadbeheer" element={<Homepage />} />
 			</Routes>
